61acd06d7a516612437c853f8f1dff1446a6a53a
marc-antoine ourradour <ourradourm@gmail.com>
2024-04-22 09:03:13 +0200

    dasboard page

--- app/(dashboard)/(routes)/layout.tsx
+++ app/(dashboard)/(routes)/layout.tsx
@@ -9,15 +9,13 @@ type DashboardLayoutProps = {
 const DashboardLayout = async ({ children }: DashboardLayoutProps) => {
   return (
     <>
-      <div className="flex flex-col min-h-screen">
-        <div className="flex-1 items-start">
-          <main className="flex flex-col w-full overflow-hidden">
-            <Header />
-            <Separator className="border" />
+      <div className="h-full relative">
+        <main className="overflow-hidden h-full flex flex-col">
+          <Header />
+          <Separator className="border" />
 
-            <div>{children}</div>
-          </main>
-        </div>
+          <div className="flex-1">{children}</div>
+        </main>
       </div>
     </>
   );
--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,7 +1,34 @@
+import Image from "next/image";
 import React from "react";
-
+import river from "@/lib/assets/designs/river-removebg-preview.png";
+import coins from "@/lib/assets/designs/coins.png";
 const Page = () => {
-  return <div>dashboardPage</div>;
+  return (
+    <div className="relative h-full flex justify-center">
+      <div className="flex mt-[17vh] text-5xl font-light leading-[3.5rem] word-spacing-wide">
+        <span>
+          <span className="font-semibold"> miki </span> allow users to
+          efficiently and <br />
+          effortlessly create, trade, and lend <br />
+          liquidity through one powerful platform.
+        </span>
+      </div>
+      <div className="opacity-35 ">
+        <Image
+          src={river}
+          alt="river"
+          height={700}
+          className="absolute left-[-20%] bottom-[-50%] rotate-45"
+        />
+      </div>
+      <Image
+        className="absolute left-[12%] bottom-[9%]"
+        src={coins}
+        alt="coins"
+        height={340}
+      />
+    </div>
+  );
 };
 
 export default Page;
