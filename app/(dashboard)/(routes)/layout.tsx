a2769c5736fc39fb500e363943ffd0821e7262a8
marc-antoine ourradour <ourradourm@gmail.com>
2024-04-25 04:23:48 +0200

    add push link  to miki logo

--- app/(dashboard)/(routes)/layout.tsx
+++ app/(dashboard)/(routes)/layout.tsx
@@ -1,5 +1,4 @@
 import Header from "@/components/header/header";
-import { Separator } from "@/components/ui/separator";
 import React from "react";
 
 type DashboardLayoutProps = {
--- components/header/header.tsx
+++ components/header/header.tsx
@@ -2,11 +2,21 @@
 import React from "react";
 import Menu from "./menu/menu";
 import AddressDiplayed from "./addressDiplayed";
+import { useRouter } from "next/navigation";
 
 const Header = () => {
+  const { push } = useRouter();
   return (
     <div className="flex items-center justify-between w-full px-12 py-4 gap-4 ">
-      <div className="font-medium text-3xl">miki</div>
+      {/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
+      <div
+        className="font-medium text-3xl cursor-pointer"
+        onClick={() => {
+          push("/");
+        }}
+      >
+        miki
+      </div>
       <div className="flex flex-row items-center space-x-4">
         <Menu />
         <AddressDiplayed />
