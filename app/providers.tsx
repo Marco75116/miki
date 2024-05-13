b858130eae83f575e614dc9c77b3c4b71218a710
marc-antoine ourradour <ourradourm@gmail.com>
2024-05-13 06:20:36 +0200

    add apollo client + component example transfers

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,14 +4,22 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { WagmiProvider } from "wagmi";
 import { config } from "@/lib/clients/wagmi/config";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+	uri: "http://localhost:4350/graphql",
+	cache: new InMemoryCache(),
+});
 
 export function Providers({ children }: { children: ReactNode }) {
-  const queryClient = new QueryClient();
-  return (
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <TooltipProvider>{children}</TooltipProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
-  );
+	const queryClient = new QueryClient();
+	return (
+		<ApolloProvider client={client}>
+			<WagmiProvider config={config}>
+				<QueryClientProvider client={queryClient}>
+					<TooltipProvider>{children}</TooltipProvider>
+				</QueryClientProvider>
+			</WagmiProvider>
+		</ApolloProvider>
+	);
 }
---
+++ components/transfer.tsx
@@ -0,0 +1,33 @@
+"use client";
+import React from "react";
+import { useQuery, gql } from "@apollo/client";
+
+const TRANSFERS_QUERY = gql`
+  query MyQuery {
+    transfers {
+      from
+      id
+      to
+      value
+    }
+  }
+`;
+const Transfers = () => {
+	const { loading, error, data } = useQuery(TRANSFERS_QUERY);
+
+	if (loading) return <p>Loading...</p>;
+	if (error) return <p>Error :{error.message}</p>;
+
+	return (
+		<div>
+			<h1>Transfers</h1>
+			<ul>
+				{data.transfers.map(({ id, from, to, value }) => (
+					<li key={id}>{`From: ${from}, To: ${to}, Value: ${value}`}</li>
+				))}
+			</ul>
+		</div>
+	);
+};
+
+export default Transfers;
